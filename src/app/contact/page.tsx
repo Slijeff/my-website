42642015c0ad4b30033c77760c483cf9509b759d
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2025-01-02 16:20:41 -0600

    feat: add contact information

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,17 @@
-import { Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function Contact() {
-  return <Typography>Contact</Typography>;
+  return (
+    <Stack gap={2} pt={2}>
+      <Typography variant="h3" fontWeight="bold">
+        Contact Information
+      </Typography>
+      <Typography variant="h6" color="primary.light">
+        <Box component="span" fontWeight="bold">
+          Email:
+        </Box>{" "}
+        thisisjeff@duck.com
+      </Typography>
+    </Stack>
+  );
 }
