4c51d1fc195d0ea0ddc4d111655982eaf56342ee
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2025-01-12 20:19:58 -0600

    feat: add progressbar to blogs

--- src/app/blogs/template.tsx
+++ src/app/blogs/template.tsx
@@ -1,3 +1,4 @@
+import { ProgressBar } from '@/components/progressbar';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -5,5 +6,10 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <ProgressBar />
+      {children}
+    </>
+  );
 }
