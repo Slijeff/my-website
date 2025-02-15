eba8bcddb9e9ff31ea4ce0b792d5b48f078d8bf4
Jeffrey Hui <31459252+Slijeff@users.noreply.github.com>
2025-02-14 19:29:02 -0600

    fix: fix archive pagination overflow

--- src/components/archivePagination.tsx
+++ src/components/archivePagination.tsx
@@ -7,23 +7,52 @@ interface ArchivePaginationProps {
   totalPage: number;
 }
 export const ArchivePagination = ({ totalPage }: ArchivePaginationProps) => {
-  const currentPage = useParams();
+  const currentPage: { pagination: string } = useParams();
   return (
     <Pagination
       count={totalPage}
+      siblingCount={0}
+      boundaryCount={1}
       page={Number(currentPage.pagination) + 1}
       size="large"
-      renderItem={item =>
-        item.page ? (
-          <PaginationItem
-            component={Link}
-            href={`/archive/${item.page - 1}`}
-            {...item}
-          />
-        ) : (
-          <></>
-        )
-      }
+      renderItem={item => {
+        if (item.type === 'page') {
+          return (
+            <PaginationItem
+              component={Link}
+              href={`/archive/${item.page! - 1}`}
+              {...item}
+            />
+          );
+        }
+        if (item.type === 'start-ellipsis') {
+          return <PaginationItem {...item} />;
+        }
+        if (item.type === 'end-ellipsis') {
+          return <PaginationItem {...item} />;
+        }
+        if (currentPage.pagination !== '0' && item.type === 'previous') {
+          return (
+            <PaginationItem
+              component={Link}
+              href={`/archive/${item.page! - 1}`}
+              {...item}
+            />
+          );
+        }
+        if (
+          currentPage.pagination !== `${totalPage - 1}` &&
+          item.type === 'next'
+        ) {
+          return (
+            <PaginationItem
+              component={Link}
+              href={`/archive/${item.page! - 1}`}
+              {...item}
+            />
+          );
+        }
+      }}
     />
   );
 };
