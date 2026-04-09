9a0d3ab9ca1d47ee759f903f1dd0cfc8fbf887df
Jeffrey <jeff83636tx@gmail.com>
2026-04-08 21:10:21 -0500

    chore(config): simplify MDX plugin imports in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 import createMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
-import remarkToc from 'remark-toc';
-import remarkFrontmatter from 'remark-frontmatter';
-import rehypePrism from 'rehype-prism-plus';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
@@ -36,11 +31,11 @@ const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [
-      remarkGfm,
-      [remarkToc, { ordered: true }],
-      remarkFrontmatter,
+      'remark-gfm',
+      ['remark-toc', { ordered: true }],
+      'remark-frontmatter',
     ],
-    rehypePlugins: [rehypePrism, rehypeAutolinkHeadings],
+    rehypePlugins: ['rehype-prism-plus', 'rehype-autolink-headings'],
   },
 });
 
